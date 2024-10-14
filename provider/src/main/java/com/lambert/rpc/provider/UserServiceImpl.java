38c47dfc5433a581c878e4ac041df0324c61afb9
EdmitSpera <laoxiao1886@gmail.com>
2024-10-14 20:17:12 +0800

    feature: 服务接口业务功能实现

---
+++ provider/src/main/java/com/lambert/rpc/provider/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.lambert.rpc.provider;
+
+import com.lambert.rpc.common.model.User;
+import com.lambert.rpc.common.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User getUser(User user) {
+        System.out.println("用户名：" + user.getName());
+        return user;
+    }
+}
